5c7dbab9561d5f42fcdc11d3131f123c7f0a2ae8
zhaokai <ken_829@sohu.com>
2018-12-03 17:47:15 +0800

    更新主题结构

--- src/com/crscic/gate/config/IConfig.java
+++ src/com/crscic/gate/config/IConfig.java
@@ -1,13 +1,13 @@
-package com.crscic.gate.setting;
+package com.crscic.gate.config;
 
 /**
  * 
  * @author zhaokai
  * 2018年12月3日 下午1:56:52
  */
-public interface ISetting
+public interface IConfig
 {
 	public void load(String path);
 	public String getProperty(String propertyName);
-	public void test();
+	
 }
